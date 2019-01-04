2c9da73e899b41abcf1d748857e93c8457bf9e6b
kubill <meiweijia@vip.qq.com>
2019-01-04 17:47:35 +0800

    分页

--- app/Http/Controllers/Api/ApiController.php
+++ app/Http/Controllers/Api/ApiController.php
@@ -3,10 +3,11 @@
 namespace App\Http\Controllers\Api;
 
 use App\Traits\ApiResponse;
+use App\Traits\Paginator;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 
 class ApiController extends Controller
 {
-    use ApiResponse;
+    use ApiResponse,Paginator;
 }
---
+++ app/Traits/Paginator.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Traits;
+
+
+use Illuminate\Pagination\LengthAwarePaginator;
+
+trait Paginator
+{
+    /**
+     * @param LengthAwarePaginator $paginator
+     * @return array
+     */
+    protected function paginate(LengthAwarePaginator $paginator){
+        return [
+            'current_page' => $paginator->currentPage(),
+            'last_page' => $paginator->lastPage(),
+            'per_page' => $paginator->perPage(),
+            'total' => $paginator->total(),
+            'data' => $paginator->items(),
+        ];
+    }
+}
