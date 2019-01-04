bed198ac7764f3eb6854b3653d89231e61e824da
kubill <meiweijia@vip.qq.com>
2019-01-04 10:09:41 +0800

    增加日志记录

---
+++ app/Http/Middleware/Log.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Support\Facades\DB;
+
+class Log
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure  $next
+     * @return mixed
+     */
+    public function handle($request, Closure $next)
+    {
+        DB::enableQueryLog();
+        $response = $next($request);
+        $query = DB::getQueryLog();
+        if (strpos($request->path(), 'admin/logs') !== false) {
+            return $response;
+        }
+        $log = array('url' => $request->path(), 'data' => $request->all(), 'sql' => $query, 'agent' => $request->userAgent(), 'ip' => $request->ip());
+        \Illuminate\Support\Facades\Log::info('OperationLog', $log);
+        return $response;
+    }
+}
