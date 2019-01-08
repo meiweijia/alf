c74ae9ae3ba9dca8664011ef1d0e338e58db0d82
kubill <meiweijia@vip.qq.com>
2019-01-08 15:45:31 +0800

    修改定时任务时间

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -20,14 +20,14 @@ class Kernel extends ConsoleKernel
     /**
      * Define the application's command schedule.
      *
-     * @param  \Illuminate\Console\Scheduling\Schedule  $schedule
+     * @param  \Illuminate\Console\Scheduling\Schedule $schedule
      * @return void
      */
     protected function schedule(Schedule $schedule)
     {
         // $schedule->command('inspire')
         //          ->hourly();
-        $schedule->command('SPField')->hourlyAt(39);
+        $schedule->command('SPField')->hourly();
     }
 
     /**
@@ -37,7 +37,7 @@ class Kernel extends ConsoleKernel
      */
     protected function commands()
     {
-        $this->load(__DIR__.'/Commands');
+        $this->load(__DIR__ . '/Commands');
 
         require base_path('routes/console.php');
     }
