198dd36a69457a7c4fb2242c29cee7e3c0972789
kubill <meiweijia@vip.qq.com>
2019-01-08 17:15:55 +0800

    诳语

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -20,8 +20,6 @@ class Kernel extends HttpKernel
         \Illuminate\Foundation\Http\Middleware\ConvertEmptyStringsToNull::class,
         \App\Http\Middleware\TrustProxies::class,
         \App\Http\Middleware\Log::class,
-        \App\Http\Middleware\EnableCrossRequestMiddleware::class,
-
     ];
 
     /**
@@ -43,6 +41,7 @@ class Kernel extends HttpKernel
         'api' => [
             'throttle:60,1',
             'bindings',
+            \Barryvdh\Cors\HandleCors::class,
         ],
     ];
 
