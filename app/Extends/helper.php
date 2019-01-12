478d30ebd83f632f0da8f56af4b27756077dff2c
kubill <meiweijia@vip.qq.com>
2019-01-12 18:25:24 +0800

    添加星期和中文的映射

--- app/Extends/helper.php
+++ app/Extends/helper.php
@@ -29,3 +29,24 @@ if (!function_exists('week_day_map')) {
         return $data;
     }
 }
+
+if (!function_exists('week_map')) {
+    /**
+     * 星期的映射
+     *
+     * @return array
+     */
+    function week_map($v)
+    {
+        $map = [
+            0 => '周日',
+            1 => '周一',
+            2 => '周二',
+            3 => '周三',
+            4 => '周四',
+            5 => '周五',
+            6 => '周六',
+        ];
+        return $map[$v];
+    }
+}
