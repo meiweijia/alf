e8f29adb3055e330d7c72d2c03c52e83df1acbcd
kubill <meiweijia@vip.qq.com>
2019-01-18 11:16:30 +0800

    关注消息修改

--- app/Http/Controllers/WechatController.php
+++ app/Http/Controllers/WechatController.php
@@ -9,6 +9,7 @@ class WechatController extends Controller
 {
     public function serve()
     {
-        return app(Wechat::class)->serve('澳莱芙');
+        $msg = '澳莱芙球馆！'.PHP_EOL.'温馨提示：绑定手机号可以接收订场确认短信；充值会员也可以通过手机号找回。';
+        return app(Wechat::class)->serve($msg);
     }
 }
